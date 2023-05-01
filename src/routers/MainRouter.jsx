d49ca6cb78939d537d1d12ddfa96060a4f06107b
Anis1020 <anis.dapunia@gmail.com>
2023-05-02 00:32:58 +0600

    layout setup done

---
+++ src/routers/MainRouter.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
+import Home from "../Components/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+    ],
+  },
+]);
+
+export default router;
